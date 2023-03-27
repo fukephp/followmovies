e73fb73d6d5d75c0a8d2c6ef9689e67ffb7a524a
farukh <faruk.hopic@gmail.com>
2023-03-27 10:04:39 +0200

    Install new package elquent slugabble Update create movies table migration

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Movie extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     /**
      * The attributes that are mass assignable.
@@ -22,4 +23,23 @@ class Movie extends Model
         'vote_count',
         'released_at',
     ];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'movieslug'
+            ]
+        ];
+    }
+
+    public function getMovieSlugAttribute(): string
+    {
+        return $this->title . '-movie';
+    }
 }
--- database/migrations/2023_03_26_215836_create_movies_table.php
+++ database/migrations/2023_03_26_215836_create_movies_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('slug')->unique();
             $table->text('caption')->nullable();
             $table->string('image_url')->nullable();
             $table->float('rating', 8, 1)->nullable()->default(0);
             $table->integer('vote_count')->nullable()->default(0);
             $table->date('released_at')->nullable();
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamps();
 
         });
     }
